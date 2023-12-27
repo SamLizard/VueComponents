c114a55396266c75c4b1c6159def6d05b2065bd8
noa hacohen <noanoa112@gmail.com>
2023-12-27 15:24:21 +0200

    feat(componentView): added translation to other swal popup parts.

--- global-components/src/views/ComponentsView.vue
+++ global-components/src/views/ComponentsView.vue
@@ -3,6 +3,15 @@
     <date-picker-usage />
     <time-picker-usage />
     <language-selection />
+    <popup-structure ref="testPopup" :title="$t('popup')" :proceed="proceed">
+      <template v-slot:content>
+        <div class="mb-5">
+          <v-icon class="my-5" size="5rem">add</v-icon>
+        </div>
+      </template>
+    </popup-structure>
+    <v-btn @click="$refs.testPopup.show()" :title="$t('regularPopup')">{{$t("show")}}</v-btn>
+    <v-btn @click="openSweetalert" :title="$t('sweetalertPopup')">{{$t("show")}}</v-btn>
   </div>
 </template>
 
@@ -10,6 +19,7 @@
 import DatePickerUsage from "../componentsUsages/DatePickerUsage.vue";
 import TimePickerUsage from "../componentsUsages/TimePickerUsage.vue";
 import LanguageSelection from "../components/LanguageSelection.vue";
+import PopupStructure from "../components/PopupStructure.vue";
 
 export default {
   name: "ComponentsView",
@@ -17,11 +27,39 @@ export default {
     DatePickerUsage,
     TimePickerUsage,
     LanguageSelection,
+    PopupStructure,
   },
   data() {
     return {};
   },
-  methods: {},
+  methods: {
+    proceed() {
+      alert("You made it!");
+    },
+    openSweetalert() {
+      this.$swal
+        .fire({
+          title: this.$t("confirmationQuestion"),
+          // text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: `${this.$t("proceed")}`,
+          cancelButtonText: `${this.$t("cancel")}`,
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            this.$swal.fire({
+              title: this.$t("deleted") + "!",
+              text: this.$t("deletedFileMessage"),
+              icon: "success",
+              confirmButtonText: this.$t("ok"),
+            });
+          }
+        });
+    },
+  },
   computed: {},
 };
 </script>
--- global-components/src/views/HomeView.vue
+++ global-components/src/views/HomeView.vue
@@ -1,53 +1,17 @@
 <template>
   <div>
-    <popup-structure ref="testPopup" :title="$t('popup')" :proceed="proceed">
-      <template v-slot:content>
-        <div class="mb-5">
-          <v-icon class="my-5" size="5rem">add</v-icon>
-        </div>
-      </template>
-    </popup-structure>
-    <v-btn @click="$refs.testPopup.show()" :title="$t('regularPopup')" >{{$t('show')}}</v-btn>
-    <v-btn @click="openSweetalert" :title="$t('sweetalertPopup')" >{{$t('show')}}</v-btn>
   </div>
 </template>
 
 <script>
-import PopupStructure from '../components/PopupStructure.vue';
 
 export default {
   name: "HomeView",
-  components: {
-    PopupStructure,
-  },
+  components: {},
   data() {
     return {};
   },
-  methods: {
-    proceed() {
-      alert('You made it!');
-    },
-    openSweetalert() {
-      this.$swal.fire({
-        title: `${this.$t('confirmationQuestion')}`,
-        // text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: `${this.$t('proceed')}`,
-        cancelButtonText: `${this.$t('cancel')}`,
-      }).then((result) => {
-        if (result.isConfirmed) {
-          this.$swal.fire({
-            title: `${this.$t('deleted')}` + '!',
-            text: `${this.$t('deletedFileMessage')}`,
-            icon: "success"
-          });
-        }
-      });
-    },
-  },
+  methods: {},
   computed: {},
 };
 </script>
